9a530af2fafc784621549e60617522e95eccc0fc
Humam-Dwikat <91946608+Humam-Dwikat@users.noreply.github.com>
2024-10-27 14:20:09 +0200

    Time: 26 ms (93.01%) | Memory: 18.7 MB (35.12%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        point1 = head
+        point2 = head
+
+        while point2 and point2.next:
+            point1 = point1.next           
+            point2 = point2.next.next      
+
+            if point1 == point2:          
+                return True
+
+        return False           
